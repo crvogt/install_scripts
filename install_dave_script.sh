31edb80d135199c2919bba1addaa68e1dd531fb9
crvogt <crvogt26@gmail.com>
2021-06-15 15:37:40 -0700

    modified dave script install

--- install_dave_script.sh
+++ install_dave_script.sh
@@ -1,16 +1,16 @@
-echo 'Installing Dave and dependencies'
-
 USER='carson'
-ROS_VERSION='noetic'
-GAZEBO_VERSION='gazebo11'
+ROS_VERSION='melodic'
+GAZEBO_VERSION='gazebo9'
 INSTALL_VRX='true'
 INSTALL_DAVE=''
 INSTALL_DAVE_MBS=''
-sudo apt-get update
 
-if [[ -n INSTALL_DAVE ]]
+if [[ -n $INSTALL_DAVE ]]
 then
+	echo "Installing Dave and dependencies"
+	sleep 3s
 	# Installing dependencies
+	sudo apt-get update
 	sudo apt-get install -y build-essential cppcheck curl cmake lsb-release git python3-dbg python3-pip python3-venv ruby software-properties-common wget libeigen3-dev pkg-config protobuf-compiler qtbase5-dev libgles2-mesa-dev -y
 
 	sudo apt-get install ros-$ROS_VERSION-rqt-robot-plugins ros-$ROS_VERSION-effort-controllers ros-$ROS_VERSION-joy ros-$ROS_VERSION-teleop-twist-joy ros-$ROS_VERSION-teleop-twist-keyboard ros-$ROS_VERSION-teleop-tools ros-$ROS_VERSION-joy-teleop ros-$ROS_VERSION-key-teleop ros-$ROS_VERSION-geographic-info ros-$ROS_VERSION-move-base ros-$ROS_VERSION-robot-localization ros-$ROS_VERSION-robot-state-publisher ros-$ROS_VERSION-xacro ros-$ROS_VERSION-rqt ros-$ROS_VERSION-rqt-common-plugins ros-$ROS_VERSION-hector-gazebo-plugins -y
@@ -34,8 +34,11 @@ fi
 
 if [[ -n $INSTALL_VRX ]]
 then
+	echo "Installing VRX and dependencies"
+	sleep 3s
 	# Install dependencies
-	sudo apt install cmake mercurial git ruby libeigen3-dev lib$GAZEBO_VERSION-dev pkg-config python ros-$ROS_VERSION-gazebo-plugins ros-${DIST}-gazebo-ros ros-$ROS_VERSION-hector-gazebo-plugins ros-$ROS_VERSION-joy ros-$ROS_VERSION-joy-teleop ros-$ROS_VERSION-key-teleop ros-$ROS_VERSION-robot-localization ros-$ROS_VERSION-robot-state-publisher ros-$ROS_VERSION-joint-state-publisher ros-$ROS_VERSION-rviz ros-$ROS_VERSION-ros-base ros-$ROS_VERSION-teleop-tools ros-$ROS_VERSION-teleop-twist-keyboard ros-$ROS_VERSION-velodyne-simulator ros-$ROS_VERSION-xacro ros-$ROS_VERSION-rqt ros-$ROS_VERSION-rqt-common-plugins protobuf-compiler
+	sudo apt-get update
+	sudo apt install cmake mercurial git ruby libeigen3-dev lib$GAZEBO_VERSION-dev pkg-config python ros-$ROS_VERSION-gazebo-plugins ros-$ROS_VERSION-gazebo-ros ros-$ROS_VERSION-hector-gazebo-plugins ros-$ROS_VERSION-joy ros-$ROS_VERSION-joy-teleop ros-$ROS_VERSION-key-teleop ros-$ROS_VERSION-robot-localization ros-$ROS_VERSION-robot-state-publisher ros-$ROS_VERSION-joint-state-publisher ros-$ROS_VERSION-rviz ros-$ROS_VERSION-ros-base ros-$ROS_VERSION-teleop-tools ros-$ROS_VERSION-teleop-twist-keyboard ros-$ROS_VERSION-velodyne-simulator ros-$ROS_VERSION-xacro ros-$ROS_VERSION-rqt ros-$ROS_VERSION-rqt-common-plugins protobuf-compiler -y
 
 
 	mkdir -p /home/$USER/vrx_ws/src

acc12efeefe8bcbc6e78aa1b0f0d7706da9c4b58
crvogt <crvogt26@gmail.com>
2022-05-25 17:15:55 -0700

    Added vimrc creation mechs

--- startup_install_script.sh
+++ startup_install_script.sh
@@ -58,6 +58,20 @@ echo 'bind -n M-Right select-pane -R' >> .tmux.conf
 echo 'bind -n M-Up select-pane -U' >> .tmux.conf
 echo 'bind -n M-Down select-pane -D' >> .tmux.conf
 
+touch .vimrc
+echo 'syntax on' >> .vimrc
+echo 'filetype plugin indent on' >> .vimrc
+echo 'set tabstop=2' >> .vimrc
+echo 'set shiftwidth=2' >> .vimrc
+echo 'set expandtab' >> .vimrc
+echo 'colorscheme desert' >> .vimrc
+echo 'set foldmethod=indent' >> .vimrc
+echo 'autocmd BufRead * normal zR' >> .vimrc
+echo 'set wildmenu' >> .vimrc
+echo 'set number' >> .vimrc
+echo 'set hlsearch' >> .vimrc
+echo 'highlight Search guibg='Purple' guifg='NONE'' >> .vimrc
+
 if [[ -n $INSTALL_ROS  ]]
 then
 	install_ros_script.sh
---
+++ vim_gen.sh
@@ -0,0 +1,13 @@
+touch .vimrc
+echo 'syntax on' >> .vimrc
+echo 'filetype plugin indent on' >> .vimrc
+echo 'set tabstop=2' >> .vimrc
+echo 'set shiftwidth=2' >> .vimrc
+echo 'set expandtab' >> .vimrc
+echo 'colorscheme desert' >> .vimrc
+echo 'set foldmethod=indent' >> .vimrc
+echo 'autocmd BufRead * normal zR' >> .vimrc
+echo 'set wildmenu' >> .vimrc
+echo 'set number' >> .vimrc
+echo 'set hlsearch' >> .vimrc
+echo 'highlight Search guibg='Purple' guifg='NONE'' >> .vimrc
